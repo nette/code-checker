2812833c8bf792b69939597126d398c6aa4ff941
David Grudl <david@grudl.com>
2010-11-10 00:24:33 +0100

    Code-Checker: refactoring

--- Code-Checker/code-checker.php
+++ Code-Checker/code-checker.php
@@ -32,91 +32,153 @@ Options:
 <?php
 }
 
-// configuration
-$accept = array(
-	'*.php', '*.phpc', '*.phpt', '*.inc',
-	'*.txt', '*.texy',
-	'*.css', '*.js', '*.htm', '*.html', '*.phtml', '*.xml',
-	'*.ini', '*.config',
-	'*.sh',	'*.bat',
-	'.htaccess', '.gitignore',
-);
-
-$ignore = array(
-	'.*', '*.tmp', 'tmp', 'temp', 'log',
-);
-
-$folder = isset($options['d']) ? $options['d'] : getcwd();
 
 
+class CodeChecker extends Nette\Object
+{
+	public $tasks = array();
 
-// execution
-set_time_limit(0);
+	public $readOnly = FALSE;
 
-if (!isset($options['f'])) {
-	echo "Running in read-only mode\n";
+	public $accept = array(
+		'*.php', '*.phpc', '*.phpt', '*.inc',
+		'*.txt', '*.texy',
+		'*.css', '*.js', '*.htm', '*.html', '*.phtml', '*.xml',
+		'*.ini', '*.config',
+		'*.sh',	'*.bat',
+		'.htaccess', '.gitignore',
+	);
+
+	public $ignore = array(
+		'.*', '*.tmp', 'tmp', 'temp', 'log',
+	);
+
+	private $file;
+
+	private $error;
+
+
+	public function run($folder)
+	{
+		set_time_limit(0);
+
+		if ($this->readOnly) {
+			echo "Running in read-only mode\n";
+		}
+
+		echo "Scanning folder $folder...\n";
+
+		$counter = 0;
+		foreach (Nette\Finder::findFiles($this->accept)->from($folder)->exclude($this->ignore) as $file)
+		{
+			echo str_pad(str_repeat('.', $counter++ % 40), 40), "\x0D";
+
+			$orig = $s = file_get_contents($file);
+			$this->file = ltrim(str_replace($folder, '', $file), '/\\');
+			$this->error = FALSE;
+
+			foreach ($this->tasks as $task) {
+				$res = $task($this, $s);
+				if ($this->error) {
+					continue 2;
+				} elseif (is_string($res)) {
+					$s = $res;
+				}
+			}
+
+			if ($s !== $orig && !$this->readOnly) {
+				file_put_contents($file, $s);
+			}
+		}
+
+		echo "\nDone.";
+	}
+
+
+	public function fix($message)
+	{
+		echo '[' . ($this->readOnly ? 'FOUND' : 'FIX') . "] $this->file   $message\n";
+	}
+
+
+	public function warning($message)
+	{
+		echo "[WARNING] $this->file   $message\n";
+	}
+
+
+	public function error($message)
+	{
+		echo "[ERROR] $this->file   $message\n";
+		$this->error = TRUE;
+	}
+
+
+	public function is($extension)
+	{
+		return $extension === pathinfo($this->file, PATHINFO_EXTENSION);
+	}
+
 }
 
-echo "Scanning folder $folder...\n";
 
-$counter = 0;
-foreach (Nette\Finder::findFiles($accept)->from($folder)->exclude($ignore) as $file)
-{
-	echo str_pad(str_repeat('.', $counter++ % 40), 40), "\x0D";
 
-    $orig = $s = file_get_contents($file);
-    $shortName = ltrim(str_replace($folder, '', $file), '/\\') . '  ';
-    $ext = pathinfo($file, PATHINFO_EXTENSION);
+$checker = new CodeChecker;
+$checker->readOnly = !isset($options['f']);
 
-	// search for control characters
+// control characters checker
+$checker->tasks[] = function($checker, $s) {
 	if (String::match($s, '#[\x00-\x08\x0B\x0C\x0E-\x1F]#')) {
-		echo "[ERROR] $shortName contains control characters\n";
-		continue;
+		$checker->error('contains control characters');
 	}
+};
 
-    // remove BOM
+// BOM remover
+$checker->tasks[] = function($checker, $s) {
     if (substr($s, 0, 3) === "\xEF\xBB\xBF") {
-    	$s = substr($s, 3);
-		echo "[FIX] $shortName contains BOM\n";
+    	$checker->fix('contains BOM');
+    	return substr($s, 3);
     }
+};
 
-	// search for UTF-8 characters
+// UTF-8 checker
+$checker->tasks[] = function($checker, $s) {
 	if (!String::checkEncoding($s)) {
-		echo "[ERROR] $shortName in not valid UTF-8 file\n";
-		continue;
+		$checker->error('in not valid UTF-8 file');
 	}
+};
 
-	// convert newline characters for the current OS
-	if (isset($options['l'])) {
-		$old = $s;
-		$s = str_replace("\n", PHP_EOL, str_replace("\r\n", "\n", $s));
-		if ($old !== $s) {
-			echo "[FIX] $shortName contains non-system line-endings\n";
+// newline characters normalizer for the current OS
+if (isset($options['l'])) {
+	$checker->tasks[] = function($checker, $s) {
+		$new = str_replace("\n", PHP_EOL, str_replace("\r\n", "\n", $s));
+		if ($new !== $s) {
+    		$checker->fix('contains non-system line-endings');
+    		return $new;
 		}
-	}
+	};
+}
 
-    if ($ext === 'php') {
-		// remove trailing ? >
+// trailing ? > remover
+$checker->tasks[] = function($checker, $s) {
+    if ($checker->is('php')) {
 		$tmp = rtrim($s);
 		if (substr($tmp, -2) === '?>') {
-			$s = substr($tmp, 0, -2);
-			echo "[FIX] $shortName contains closing PHP tag ?>\n";
+    		$checker->fix('contains closing PHP tag ?>');
+			return substr($tmp, 0, -2);
 		}
     }
+};
 
-	// trim white-space
-	$old = $s;
-    $s = String::replace($s, "#[\t ]+(\r?\n)#", '$1'); // right trim
-    $s = String::replace($s, "#(\r?\n)+$#", '$1'); // trailing trim
-    if ($old !== $s) {
-    	$bytes = strlen($old) - strlen($s);
-		echo "[FIX] $shortName $bytes bytes of whitespaces\n";
+// white-space remover
+$checker->tasks[] = function($checker, $s) {
+    $new = String::replace($s, "#[\t ]+(\r?\n)#", '$1'); // right trim
+    $new = String::replace($new, "#(\r?\n)+$#", '$1'); // trailing trim
+    if ($new !== $s) {
+    	$bytes = strlen($s) - strlen($new);
+   		$checker->fix("$bytes bytes of whitespaces");
+   		return $new;
    	}
+};
 
-    // save if option -f is present
-    if ($s !== $orig && isset($options['f'])) {
-		file_put_contents($file, $s);
-   	}
-}
-
-echo "\nDone. ";
+$checker->run(isset($options['d']) ? $options['d'] : getcwd());
