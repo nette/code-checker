d86cbeb9bf0c92183e01bfaf9695429276b1f3ac
David Grudl <david@grudl.com>
2012-11-21 17:37:30 +0100

    Requirements-Checker: minimal PHP version is 5.3.0

--- Requirements-Checker/checker.php
+++ Requirements-Checker/checker.php
@@ -47,9 +47,9 @@ $tests[] = array(
 $tests[] = array(
 	'title' => 'PHP version',
 	'required' => TRUE,
-	'passed' => version_compare(PHP_VERSION, '5.2.0', '>='),
+	'passed' => version_compare(PHP_VERSION, '5.3.0', '>='),
 	'message' => PHP_VERSION,
-	'description' => 'Your PHP version is too old. Nette Framework requires at least PHP 5.2.0 or higher.',
+	'description' => 'Your PHP version is too old. Nette Framework requires at least PHP 5.3.0 or higher.',
 );
 
 $tests[] = array(
