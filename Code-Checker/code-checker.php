03f250aa82012bcd1b03409b375979561f0ee7a1
David Grudl <david@grudl.com>
2013-12-23 16:16:41 +0100

    typo

--- Code-Checker/code-checker.php
+++ Code-Checker/code-checker.php
@@ -9,7 +9,7 @@
  * the file license.txt that was distributed with this source code.
  */
 
-require __DIR__ . '/../../Nette-minified/nette.min.php';
+require __DIR__ . '/../../Nette-minified/nette.phar';
 
 use Nette\Utils\Strings;
 
