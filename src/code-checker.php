015829abc034c93a9195103f1ae261bb3bc8caf2
David Grudl <david@grudl.com>
2017-02-27 12:25:57 +0100

    better error message for indentation checking [Closes #31][Closes #32]

--- src/code-checker.php
+++ src/code-checker.php
@@ -382,9 +382,12 @@ $checker->tasks[] = function ($s) {
 			$s = $res;
 		}
 		$offset = 0;
-		if (preg_match('#^\t*+\ (?!\*)\s*#m', $s, $m, PREG_OFFSET_CAPTURE)) {
+		if (preg_match('#^(\t*+)\ (?!\*)\s*#m', $s, $m, PREG_OFFSET_CAPTURE)) {
 			$line = $m[0][1] ? substr_count($orig, "\n", 0, $m[0][1]) + 1 : 1;
-			$this->error('Mixed tabs and spaces indentation', $line);
+			$this->error(
+				$m[1][0] ? 'Mixed tabs and spaces to indent' : 'Used space to indent instead of tab',
+				$line
+			);
 			$offset = $m[0][1] + strlen($m[0][0]) + 1;
 		}
 		if (preg_match('#(?<=[\S ])(?<!^//)\t#m', $s, $m, PREG_OFFSET_CAPTURE, $offset)) {
