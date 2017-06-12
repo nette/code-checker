473cdac2cd14003744913b7152ec92de7e5d85ff
David Grudl <david@grudl.com>
2017-06-12 20:01:49 +0200

    old src/code-checker.php triggers error

--- src/code-checker.php
+++ src/code-checker.php
@@ -1,6 +1,7 @@
 <?php
-// for back compatibility
 
 declare(strict_types=1);
+
+trigger_error('CodeChecker was moved from /src/code-checker.php to /code-checker', E_USER_WARNING);
 
 require __DIR__ . '/bootstrap.php';
